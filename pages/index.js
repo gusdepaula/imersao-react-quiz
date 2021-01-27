cda23adf9e03931e736e17e30a392e6400c5ad51
Gustavo de Paula <gdepaula@gmail.com>
2021-01-27 11:12:40 -0300

    Criar o componente <Button>

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import QuizBackground from "../src/components/QuizBackground";
 import Footer from "../src/components/Footer";
 import GitHubCorner from "../src/components/GitHubCorner";
 import Input from "../src/components/Input";
+import Button from "../src/components/Button";
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -95,9 +96,9 @@ export default function Home() {
                 placeholder="Diz ai seu nome"
                 value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
