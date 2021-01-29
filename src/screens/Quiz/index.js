ed88461ab1f771f2f390c86f72e06b678bdcd79a
Gustavo de Paula <gdepaula@gmail.com>
2021-01-29 12:51:24 -0300

    Criar rotas com parâmetros dinâmicos com Next.js

--- src/screens/Quiz/index.js
+++ src/screens/Quiz/index.js
@@ -1,36 +1,40 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import db from "./db.json";
-import Widget from "../src/components/Widget";
-import QuizLogo from "../src/components/QuizLogo";
-import QuizBackground from "../src/components/QuizBackground";
-import QuizContainer from "../src/components/QuizContainer";
-import AlternativesForm from "../src/components/AlternativesForm";
-import Button from "../src/components/Button";
-import ButtonLogo from "../src/components/ButtonLogo";
-import Loading from "../src/components/Loading";
+import { Lottie } from "@crello/react-lottie";
+// import db from '../../../db.json';
+import Widget from "../../components/Widget";
+import QuizLogo from "../../components/QuizLogo";
+import QuizBackground from "../../components/QuizBackground";
+import QuizContainer from "../../components/QuizContainer";
+import AlternativesForm from "../../components/AlternativesForm";
+import Button from "../../components/Button";
+import BackLinkArrow from "../../components/BackLinkArrow";
+import ButtonLogo from "../../components/ButtonLogo";
+
+import loadingAnimation from "./animations/loading.json";
 
 function ResultWidget({ results }) {
   return (
     <Widget>
-      <Widget.Header>Tela de Resultado</Widget.Header>
+      <Widget.Header>Tela de Resultado:</Widget.Header>
 
       <Widget.Content>
         <p>
           Você acertou{" "}
-          {results.reduce((somatoriaAtual, resultAtual) => {
+          {/* {results.reduce((somatoriaAtual, resultAtual) => {
             const isAcerto = resultAtual === true;
             if (isAcerto) {
               return somatoriaAtual + 1;
             }
             return somatoriaAtual;
-          }, 0)}{" "}
-          questões!
+          }, 0)} */}
+          {results.filter((x) => x).length} perguntas
         </p>
         <ul>
           {results.map((result, index) => (
-            <li key={`result__${index}`}>
-              #{index + 1} Resultado: {result === true ? "Acertou" : "Errou"}
+            <li key={`result__${result}`}>
+              #{index + 1} Resultado:
+              {result === true ? "Acertou" : "Errou"}
             </li>
           ))}
         </ul>
@@ -44,8 +48,17 @@ function LoadingWidget() {
     <Widget>
       <Widget.Header>Carregando...</Widget.Header>
 
-      <Widget.Content>
-        <Loading />
+      <Widget.Content style={{ display: "flex", justifyContent: "center" }}>
+        <Lottie
+          width="200px"
+          height="200px"
+          className="lottie-container basic"
+          config={{
+            animationData: loadingAnimation,
+            loop: true,
+            autoplay: true,
+          }}
+        />
       </Widget.Content>
     </Widget>
   );
@@ -56,21 +69,23 @@ function QuestionWidget({
   questionIndex,
   totalQuestions,
   onSubmit,
-  addResults,
+  addResult,
 }) {
   const [selectedAlternative, setSelectedAlternative] = React.useState(
     undefined
   );
-  const [isQuestionSubmited, setIsQuestionSubmited] = React.useState();
+  const [isQuestionSubmited, setIsQuestionSubmited] = React.useState(false);
   const questionId = `question__${questionIndex}`;
   const isCorrect = selectedAlternative === question.answer;
   const hasAlternativeSelected = selectedAlternative !== undefined;
+
   return (
     <Widget>
       <Widget.Header>
-        {/* <BackLinkArrow href="/" /> */}
+        <BackLinkArrow href="/" />
         <h3>{`Pergunta ${questionIndex + 1} de ${totalQuestions}`}</h3>
       </Widget.Header>
+
       <img
         alt="Descrição"
         style={{
@@ -83,12 +98,13 @@ function QuestionWidget({
       <Widget.Content>
         <h2>{question.title}</h2>
         <p>{question.description}</p>
+
         <AlternativesForm
           onSubmit={(infosDoEvento) => {
             infosDoEvento.preventDefault();
             setIsQuestionSubmited(true);
             setTimeout(() => {
-              addResults(isCorrect);
+              addResult(isCorrect);
               onSubmit();
               setIsQuestionSubmited(false);
               setSelectedAlternative(undefined);
@@ -111,18 +127,22 @@ function QuestionWidget({
                   style={{ display: "none" }}
                   id={alternativeId}
                   name={questionId}
-                  type="radio"
                   onChange={() => setSelectedAlternative(alternativeIndex)}
+                  type="radio"
                 />
                 {alternative}
               </Widget.Topic>
             );
           })}
-          {isQuestionSubmited && isCorrect && <p>Você acertou!</p>}
-          {isQuestionSubmited && !isCorrect && <p>Você errou!</p>}
+
+          {/* <pre>
+            {JSON.stringify(question, null, 4)}
+          </pre> */}
           <Button type="submit" disabled={!hasAlternativeSelected}>
             Confirmar
           </Button>
+          {isQuestionSubmited && isCorrect && <p>Você acertou!</p>}
+          {isQuestionSubmited && !isCorrect && <p>Você errou!</p>}
         </AlternativesForm>
       </Widget.Content>
     </Widget>
@@ -134,15 +154,17 @@ const screenStates = {
   LOADING: "LOADING",
   RESULT: "RESULT",
 };
-export default function QuizPage() {
+export default function QuizPage({ externalQuestions, externalBg }) {
   const [screenState, setScreenState] = React.useState(screenStates.LOADING);
   const [results, setResults] = React.useState([]);
-  const totalQuestions = db.questions.length;
   const [currentQuestion, setCurrentQuestion] = React.useState(0);
   const questionIndex = currentQuestion;
-  const question = db.questions[questionIndex];
+  const question = externalQuestions[questionIndex];
+  const totalQuestions = externalQuestions.length;
+  const bg = externalBg;
 
-  function addResults(result) {
+  function addResult(result) {
+    // results.push(result);
     setResults([...results, result]);
   }
 
@@ -154,7 +176,7 @@ export default function QuizPage() {
     // fetch() ...
     setTimeout(() => {
       setScreenState(screenStates.QUIZ);
-    }, 1 * 1000);
+    }, 1 * 2000);
     // nasce === didMount
   }, []);
 
@@ -168,7 +190,7 @@ export default function QuizPage() {
   }
 
   return (
-    <QuizBackground backgroundImage={db.bg}>
+    <QuizBackground backgroundImage={bg}>
       <QuizContainer>
         <ButtonLogo as="a" href="/" title="AluraQuiz - GusDePaula">
           <QuizLogo />
@@ -179,7 +201,7 @@ export default function QuizPage() {
             questionIndex={questionIndex}
             totalQuestions={totalQuestions}
             onSubmit={handleSubmitQuiz}
-            addResults={addResults}
+            addResult={addResult}
           />
         )}
 
