43fc5b8630ab2de8e3b8f2c23d6a10a6143d2aea
Gustavo de Paula <gdepaula@gmail.com>
2021-01-29 11:38:50 -0300

    Listar endereços dos quizes da galera na página principal, a partir do arquivo db.json

--- pages/index.js
+++ pages/index.js
@@ -99,6 +99,27 @@ export default function Home() {
             </form>
           </Widget.Content>
         </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, "")
+                  .replace("https:", "")
+                  .replace(".vercel.app", "")
+                  .split(".");
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic href={linkExterno} target="_blank">
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
+          </Widget.Content>
+        </Widget>
         <Footer></Footer>
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/gusdepaula"></GitHubCorner>
